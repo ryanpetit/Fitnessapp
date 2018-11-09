757ea93c685738b9aa53cd3fadb93c071c1414bc
Jorge <jorge@Nathans-MacBook-Pro-3.local>
2018-11-09 13:18:20 -0800

    test

---
+++ old.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+import NavigateApp from './src/components/navigate';
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigateApp />
+    );
+  }
+}
+
