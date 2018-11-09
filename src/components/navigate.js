ee499b9ab1c7e899eba251bc62150002894b325e
chaseking123 <chaseking123@gmail.com>
2018-11-09 14:05:55 -0800

    Native-Base added, Strength made homepage for testing

--- src/components/navigate.js
+++ src/components/navigate.js
@@ -4,6 +4,7 @@ import CreateScreen from './screens/create_screen/create_mainscreen'
 import SaveScreen from './screens/save_screen/save_mainscreen'
 import ProfileScreen from './screens/profile_screen/profile_mainscreen'
 import PremadeScreen from './screens/premade_screen/premade_mainscreen';
+import StrengthScreen from './screens/Strength/Strength';
 import { createStackNavigator } from 'react-navigation'
 
 
@@ -16,9 +17,11 @@ export default class NavigateApp extends Component {
 }
 
 const NavigateScreens = createStackNavigator({
+    Strengthscreen: StrengthScreen,
     Mainscreen: MainScreen,
     Createscreen: CreateScreen,
     Premadescreen: PremadeScreen,
     Savescreen: SaveScreen,
     Profilescreen: ProfileScreen
+    
 })
